ab9a84036935d45095108677a8bc5d4b0c9eb82e
Alejandro <alejandro@smartcontract.com>
2022-06-04 11:25:33 -0700

    Fix concerts?

--- pages/concerts/index.js
+++ pages/concerts/index.js
@@ -1,60 +1,24 @@
-import ConcertCard from "../../components/ConcertCard";
-import { getSortedConcertsData } from "../../lib/concerts";
 import Hero from "../../components/Hero";
 
-export async function getStaticProps() {
-  const allConcertsData = getSortedConcertsData();
-  return {
-    props: {
-      allConcertsData,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const allConcertsData = getSortedConcertsData();
+//   return {
+//     props: {
+//       allConcertsData,
+//     },
+//   };
+// }
 
-export default function Concerts({ allConcertsData }) {
+export default function Concerts() {
   return (
     <>
       <Hero bgImage="concerts">
         <h1 className="uppercase text-white">Concerts</h1>
       </Hero>
       <div className="flex flex-col md:py-20 items-center p-8">
-        {allConcertsData.length ? (
-          allConcertsData
-            .sort((a, b) => {
-              return new Date(a.date) - new Date(b.date);
-            })
-            .map(
-              ({
-                id,
-                title,
-                date,
-                time,
-                imgSrc,
-                subtitle,
-                ticketsUrl,
-                published,
-              }) => {
-                return (
-                  published && (
-                    <ConcertCard
-                      key={id}
-                      imgSrc={imgSrc}
-                      title={title}
-                      date={date}
-                      ticketsUrl={ticketsUrl}
-                      subtitle={subtitle}
-                      time={time}
-                      path={`/concerts/${id}`}
-                    />
-                  )
-                );
-              }
-            )
-        ) : (
-          <h1 style={{ margin: "100px auto 200px", fontSize: "24px" }}>
-            Stay tuned for our upcoming up concerts....
-          </h1>
-        )}
+        <h1 style={{ margin: "100px auto 200px", fontSize: "24px" }}>
+          Stay tuned for our upcoming up concerts....
+        </h1>
       </div>
     </>
   );
