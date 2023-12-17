0e61887b486618afefdb356b5d20ed42c878adcc
Alejandro Larumbe <alejandro@smartcontract.com>
2023-12-17 18:26:08 +1300

    Add wanderlust info

--- pages/concerts/index.js
+++ pages/concerts/index.js
@@ -26,47 +26,112 @@ export default function Concerts() {
       <div className="md:w-101 md:mx-auto md:my-20 p-8">
         <div className="flex flex-col gap-6">
           <div className="flex flex-col gap-2">
-            <h2 className="text-purple text-3xl ">Faculty Concert</h2>
+            <h2 className="text-purple text-3xl ">WANDERLUST</h2>
+            <h4 className="text-purple text-xl ">
+              A chamber journey through musical landscapes
+            </h4>
 
-            <h3 className="text-xl ">
-              January 6th 2024, 6:30pm @ St Peter's Church
+            <h3 className="text-lg ">
+              January 6th 2024, 6:30pm @ St Peter's Anglican Church
             </h3>
           </div>
 
-          {/* <img src="/lockdown-quartet.png" className="w-full "></img> */}
-
-          <a className="w-full flex justify-center" target="_blank">
+          <a
+            href="https://www.eventbrite.com/e/wanderlust-tickets-778863149647?aff=oddtdtcreator
+"
+            className="w-full flex justify-center"
+            target="_blank"
+          >
             <Button>Get Tickets</Button>
           </a>
+          <img src="/wanderlust.png" className="w-full "></img>
 
-          <div className="flex flex-col mt-32 gap-2">
+          <div className="flex flex-col gap-1">
+            <p className="text-pink">Performers</p>
+            <p>
+              Amelia Taylor, <span className="italic">violin</span>
+            </p>
+            <p>
+              Alejandro Larumbe, <span className="italic">violin</span>
+            </p>
+            <p>
+              Donald Maurice, <span className="italic">viola</span>
+            </p>
+            <p>
+              Yotam Levy, <span className="italic">cello</span>
+            </p>
+            <p>
+              Maria Mo, <span className="italic">piano</span>
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-pink">Program</p>
+            <p>
+              Claire Cowan:{" "}
+              <span className="italic">wood : strings : hammers : flesh</span>
+            </p>
+            <p>
+              John Psathas: <span className="italic">Corybas</span>
+            </p>
+            <p>
+              John Psathas: <span className="italic">Aegean</span>
+            </p>
+            <p>
+              Anthony Ritchie:{" "}
+              <span className="italic">Viola Sonata, Allegro tempestuoso </span>
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-pink">About the Programme</p>
+            <p>
+              Claire Cowan's "wood : strings : hammers : flesh," with its
+              flamenco nuances, speaks in whispers of intimacy and revelation.
+              John Psathas' piano trios "Corybas" and "Aegean" interlace Greek
+              mythological and natural beauty with the nuances of New Zealand's
+              culture. The evening culminates with Anthony Ritchie’s Viola
+              Sonata, a piece that seamlessly blends the structured grace of
+              classical roots with the resonant tones of folk traditions.
+            </p>
+            <p>
+              This concert presents a symphony of stories, landscapes, and
+              histories, offering an auditory journey that transcends boundaries
+              and celebrates the rich tapestry of New Zealand's musical
+              heritage.
+            </p>
+          </div>
+
+          {/* <div className="flex flex-col mt-32 gap-2">
             <h2 className="text-purple text-3xl ">
               Lunchtime Chamber Music Concert
             </h2>
 
             <h3 className="text-xl ">January 7th 2024, 1:00pm @ TBD venue</h3>
-          </div>
+          </div> */}
 
           {/* <img src="/lockdown-quartet.png" className="w-full "></img> */}
 
-          <a
+          {/* <a
             className="w-full flex justify-center"
             target="_blank"
-            // href="https://www.eventspronto.co.nz/LockdownQuartet"
+            href="https://www.eventspronto.co.nz/LockdownQuartet"
           >
             <Button>Get Tickets</Button>
-          </a>
+          </a> */}
         </div>
 
         <div className="flex flex-col mt-32 gap-2">
           <h2 className="text-purple text-3xl ">RimuFest Student Concert</h2>
-          <p className="text-xl">
+          <p className="text-lg">
             January 9th 2024, 1:00pm @ Graham Young Youth Theatre, Tauranga Boys
             College
           </p>
           <p className="text-lg ">Enter by donation</p>
         </div>
-        <img src="/student-concert.jpg" className="w-full "></img>
+        <img
+          style={{ marginBlock: "24px" }}
+          src="/student-concert.jpg"
+          className="w-full "
+        ></img>
 
         <span>
           The RimuFest student concert is a family-friendly celebration,
